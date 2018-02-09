ca414da62ceaefdc90f9c44d8bf8e8d4a9d1322e
NeoTime <riglerjeff@gmail.com>
2018-02-08 21:23:31 -0800

    Added Speakers

--- app.js
+++ app.js
@@ -1,55 +1,33 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var stylus = require('stylus');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var reload = require('reload');
 var app = express();
+var dataFile = require('./data/data.json');
+var io = require('socket.io')();
 
-// view engine setup
 app.set('port', process.env.PORT || 3000 );
+app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.locals.siteTitle = 'Roux Meetups';
+app.locals.allSpeakers = dataFile.speakers;
 
-// specify the port
-var server = app.listen(app.get('port'), function(){
+app.use(express.static('app/public'));
+app.use(require('./routes/index'));
+app.use(require('./routes/speakers'));
+app.use(require('./routes/feedback'));
+app.use(require('./routes/api'));
+app.use(require('./routes/chat'));
+
+var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+io.attach(server);
+io.on('connection', function(socket) {
+  socket.on('postMessage', function(data) {
+    io.emit('updateMessages', data);
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+reload(server, app);
