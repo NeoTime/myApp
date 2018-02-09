4166049d1b95bbb78efa1959e66e94223d9fbc2a
NeoTime <riglerjeff@gmail.com>
2018-02-08 21:06:53 -0800

    Port set up local 3000

--- app.js
+++ app.js
@@ -12,8 +12,15 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.set('port', process.env.PORT || 3000 );
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// specify the port
+var server = app.listen(app.get('port'), function(){
+  console.log('Listening on port ' + app.get('port'));
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
